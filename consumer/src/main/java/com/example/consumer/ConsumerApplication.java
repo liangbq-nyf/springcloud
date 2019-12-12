93caaae095f080ecbe785ec15445dc49279a036e
liangbq <liangbq@asiainfo.com>
2019-12-12 14:38:19 +0800

    story#001
    项目创建

--- consumer/src/main/java/com/example/consumer/ConsumerApplication.java
+++ consumer/src/main/java/com/example/consumer/ConsumerApplication.java
@@ -14,11 +14,4 @@ public class ConsumerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConsumerApplication.class, args);
     }
-
-    @Bean
-    @LoadBalanced
-    RestTemplate restTemplate(){
-        return new RestTemplate();
-    }
-
 }
--- consumer/src/main/java/com/example/consumer/service/impl/ConsumerServiceImpl.java
+++ consumer/src/main/java/com/example/consumer/service/impl/ConsumerServiceImpl.java
@@ -2,18 +2,22 @@ package com.example.consumer.service.impl;
 
 import com.example.consumer.service.IConsumerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class ConsumerServiceImpl implements IConsumerService {
 
+    @Value("${model.produce.name}")
+    private String produceName;
+
     @Autowired
     private RestTemplate restTemplate;
 
     @Override
     public String getProduceInfo() {
-        String str = restTemplate.getForObject("http://PRODUCER/producer?name="+"中国话",String.class);
+        String str = restTemplate.getForObject("http://"+produceName+"/producer?name="+"中国话",String.class);
         return str;
     }
 }
